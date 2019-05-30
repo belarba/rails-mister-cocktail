70cd2a7e1eb422e97c2319a1ab5d558ea96b0152
Daniel Belarmino <belarba@gmail.com>
2019-05-30 15:43:53 -0300

    list cocktail and add dose

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,26 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_param)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_param
+    params.require(:cocktail).permit(:name)
+  end
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,23 @@
+class DosesController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cockail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
